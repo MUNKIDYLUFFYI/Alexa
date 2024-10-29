865c72058473049ebd843b2f7d0970961fccd60a
MUNKIDYLUFFYI <hakeemmohammed068@gmail.com>
2024-10-29 17:35:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "fJ8A3LKT#r12tX_HBKCqNJBwOw1crGnug_twqOoG1FSRWogIKAYk",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
